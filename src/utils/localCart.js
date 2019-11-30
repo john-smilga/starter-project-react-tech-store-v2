f49fc189a816c69b569d32130486d1fd9de5a45b
John <johnsmilga@gmail.com>
2019-11-30 07:39:23 -0800

    inital cart changed to computers

--- src/utils/localCart.js
+++ src/utils/localCart.js
@@ -1,26 +1,26 @@
 export default [
   {
     id: 1,
-    title: "razor blade",
-    price: 1.99,
+    title: "mac vintage computer",
+    price: 200.99,
     image:
-      "https://res.cloudinary.com/coding-addict/image/upload/v1572564510/nemo2qyllu9o0fipcxzp.png",
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1574719863/iorxuhfensblqhn6jy5d.png",
     amount: 3
   },
   {
     id: 2,
-    title: "Gillette Razor",
-    price: 9.99,
+    title: "compaq vintage computer",
+    price: 40.99,
     image:
-      "https://res.cloudinary.com/coding-addict/image/upload/v1572564463/wvgfcmbplbyyegwppc5a.png",
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1574624417/vx64ovkpz9nu98tqdtzn.png",
     amount: 5
   },
   {
     id: 3,
-    title: "Barber Razor",
-    price: 7.99,
+    title: "commodore 64 vintage computer",
+    price: 129.99,
     image:
-      "https://res.cloudinary.com/coding-addict/image/upload/v1572564572/dlmagevwczesbw05we6a.png",
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1574720064/ousbxxs7ohhddd3wz18k.png",
     amount: 4
   }
 ];
